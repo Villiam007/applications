29e394e408077bb6014dc9e0e79a3fcf515a8333
Villiam007 <natthikornb@gmail.com>
2025-04-13 00:33:21 +0700

    Add files via upload

---
+++ myproject/applications/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+
+from applications.models import Application
+from applications.owners import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+# Create your views here.
+class ApplicationsListView(OwnerListView):
+    model = Application
+    template_name = "applications/application_list.html"
+
+    def get(self, request) :
+        strval =  request.GET.get("search", False)
+        if strval :
+            # Simple title-only search
+            # __icontains for case-insensitive search
+            query = Q(title__icontains=strval)
+            query.add(Q(text__icontains=strval), Q.OR)
+            query.add(Q(tags__name__in=[strval]), Q.OR)
+            application_list = Application.objects.filter(query).select_related().distinct().order_by('-updated_at')[:10]
+        else :
+            application_list = Application.objects.all().order_by('-updated_at')[:10]
+
+        favorites = list()
+        if request.user.is_authenticated:
+            # rows = [{'id': 2}, {'id': 4} ... ]  (A list of rows)
+            rows = request.user.favorite_applications.values('id')
+            # favorites = [2, 4, ...] using list comprehension
+            favorites = [ row['id'] for row in rows ]
+        ctx = {'application_list' : application_list, 'favorites': favorites}
+        return render(request, self.template_name, ctx)
