ab7cd1597e122badffa4214b7a1c94acba454fd5
Villiam007 <natthikornb@gmail.com>
2025-04-13 21:59:29 +0700

    .

--- myproject/applications/urls.py
+++ myproject/applications/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from applications import views
-from applications.views import ProductsListView
+from applications.views import ProductsListView, ProductsCreateView
 
 app_name = 'application'
 urlpatterns = [
     path('', ProductsListView.as_view(), name='product_list'),
+    path('create/', ProductsCreateView.as_view(), name='create_product'),
+    path('<int:pk>/', views.ProductsDetailView.as_view(), name='product_detail'),
+    path('update/<int:pk>/', views.ProductsUpdateView.as_view(), name='product_update'),
+    path('delete/<int:pk>/', views.ProductsDeleteView.as_view(), name='product_delete'),
+    path('category/<str:category>/', ProductsListView.as_view(), name='product_category'),
+    path('favorites/<int:pk>', views.FavoriteListView.as_view(), name='favorite_list'),
 ]
