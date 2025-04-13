76915af51a8105eb60894cb684c85830e46adeb3
Villiam007 <natthikornb@gmail.com>
2025-04-14 03:06:38 +0700

    .

--- myproject/applications/urls.py
+++ myproject/applications/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from applications import views
-from applications.views import ProductsListView, ProductsCreateView
+from applications.views import ProductsListView, ProductsCreateView, ProductsDetailView, ProductsUpdateView, ProductsDeleteView
 
-app_name = 'application'
+app_name = 'applications'
 urlpatterns = [
     path('', ProductsListView.as_view(), name='product_list'),
     path('create/', ProductsCreateView.as_view(), name='create_product'),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('update/<int:pk>/', views.ProductsUpdateView.as_view(), name='product_update'),
     path('delete/<int:pk>/', views.ProductsDeleteView.as_view(), name='product_delete'),
     path('category/<str:category>/', ProductsListView.as_view(), name='product_category'),
-    path('favorites/<int:pk>', views.FavoriteListView.as_view(), name='favorite_list'),
+    path('favorites/<int:pk>', views.AddFavoriteView.as_view(), name='product_favorite'),
+    path('unfavorite/<int:pk>', views.DeleteFavoriteView.as_view(), name='product_unfavorite'),
 ]
--- myproject/applications/views.py
+++ myproject/applications/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.db.models import Q
+from django.urls import reverse_lazy, reverse
 
 from applications.models import Product, Order, OrderItem, Category, Favorite, Review
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -26,7 +27,7 @@ class ProductsDetailView(OwnerDetailView):
     template_name = "applications/application_detail.html"
     context_object_name = "product"
 
-class ProductsCreateView(UserPassesTestMixin, OwnerCreateView):
+class ProductsCreateView(LoginRequiredMixin, UserPassesTestMixin, OwnerCreateView):
     model = Product
     template_name = "applications/application_form.html"
     fields = ['title', 'description', 'price', 'stock', 'category']
@@ -38,3 +39,42 @@ class ProductsCreateView(UserPassesTestMixin, OwnerCreateView):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
+
+class ProductsUpdateView(LoginRequiredMixin, UserPassesTestMixin, OwnerUpdateView):
+    model = Product
+    fields = ['title', 'price', 'description', 'stock', 'category', 'picture']
+    template_name = 'applications/application_form.html'
+    success_url = reverse_lazy('applications:product_list')
+
+    def test_func(self):
+        # Only staff members can update products
+        return self.request.user.is_staff
+
+    def get(self, request, pk):
+        product = get_object_or_404(Product, id=pk)
+        form = CreateForm(instance=product)
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request, pk=None):
+        product = get_object_or_404(Product, id=pk)
+        form = CreateForm(request.POST, request.FILES or None, instance=product)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        product = form.save(commit=False)
+        product.save()
+
+        return redirect(self.success_url)
+
+class ProductsDeleteView(LoginRequiredMixin, UserPassesTestMixin, OwnerDeleteView):
+    model = Product
+    template_name = "applications/application_delete.html"
+    success_url = reverse_lazy('applications:product_list')
+
+    def test_func(self):
+        # Only staff members can delete products
+        return self.request.user.is_staff
+    
