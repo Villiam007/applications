f8e2d3f6e9516ee7040151fc8932844b55ab41f6
piradaporn <nueypiradaporn@gmail.com>
2025-04-13 17:56:33 +0700

    try debug errors using copilot

--- myproject/applications/views.py
+++ myproject/applications/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.db.models import Q
 
 from applications.models import Product, Order, OrderItem, Category, Favorite, Review
@@ -15,25 +15,17 @@ class ProductsListView(OwnerListView):
     context_object_name = "products"
 
     def get_queryset(self):
-        search_query = self.request.GET.get('search', None)
         category = self.request.GET.get('category', None)
-
-        queryset = Product.objects.all()
-
-        if search_query:
-            queryset = queryset.filter(title__icontains=search_query)
-
         if category:
-            queryset = queryset.filter(category__name=category)
-
-        return queryset
+            return Product.objects.filter(category__name=category)
+        return Product.objects.all()
 
 class ProductsDetailView(OwnerDetailView):
     model = Product
     template_name = "applications/application_detail.html"
     context_object_name = "product"
 
-class ProductsCreateView(OwnerCreateView, LoginRequiredMixin):
+class ProductsCreateView(UserPassesTestMixin, OwnerCreateView):
     model = Product
     template_name = "applications/application_form.html"
     fields = ['title', 'description', 'price', 'stock', 'category']
@@ -41,7 +33,7 @@ class ProductsCreateView(OwnerCreateView, LoginRequiredMixin):
     def test_func(self):
         # Only staff members can add products
         return self.request.user.is_staff
-
+    
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
