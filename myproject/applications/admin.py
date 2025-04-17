e56f5709596fb00c6f63def4fbb89d5ab3feb8e8
Villiam007 <natthikornb@gmail.com>
2025-04-17 14:02:08 +0700

    0

--- myproject/applications/admin.py
+++ myproject/applications/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from applications.models import Product
 
 # Register your models here.
+
+class PicAdmin(admin.ModelAdmin):
+    exclude=('picture', 'content_type')
+
+admin.site.register(Product, PicAdmin)
