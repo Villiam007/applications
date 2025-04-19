0fe3bd01d61388be049ac5ee4d7f55fbe9893709
piradaporn <nueypiradaporn@gmail.com>
2025-04-19 21:03:51 +0700

    Revert "fix picture not showing"
    This reverts commit 105ad17eac0ac9051291c0e1a316d0ab31adb7f5.

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -26,3 +26,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
+# Add this code to serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
