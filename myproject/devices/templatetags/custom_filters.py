ea29242a17ec14fd46f8320aade9f9311c0882b8
udontomyum <yoskrit04@gmail.com>
2025-04-20 15:50:34 +0700

    delete templatetags

--- myproject/devices/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def dictvalue(dictionary, key):
-    """Retrieve a value from a dictionary by key."""
-    return dictionary.get(key, 0)
