3ce76ae16f8cacdb3320723660c5d8689ba67ab9
piradaporn <nueypiradaporn@gmail.com>
2025-04-21 01:50:08 +0700

    intcomma to all price

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'devices.apps.DevicesConfig',  # Custom app for the project
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
