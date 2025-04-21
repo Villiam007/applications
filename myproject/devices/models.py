0d03dadc77c4e60b28d8f8b6207f67a0ce463a7b
udontomyum <yoskrit04@gmail.com>
2025-04-21 17:15:33 +0700

    .

--- myproject/devices/models.py
+++ myproject/devices/models.py
@@ -182,7 +182,7 @@ class UserProfile(models.Model):
     city = models.CharField(max_length=100, blank=True, null=True)
     country = models.CharField(max_length=100, blank=True, null=True)
     postal_code = models.CharField(max_length=20, blank=True, null=True)
-    
+
     def __str__(self):
         return f"Profile for {self.user.username}"
 
@@ -334,3 +334,5 @@ class Coupon(models.Model):
     
     def __str__(self):
         return self.code
+    # Add this model after your existing models
+
