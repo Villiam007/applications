f67d9b8aa84e50859b46217ed85ece12e5a09fe6
Villiam007 <natthikornb@gmail.com>
2025-05-25 23:38:49 +0700

    .

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zgr@u&*tmvy0ph_szlrnkfo7l%pz6*y!nlo!*5ys72lr7*yo@b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mactrex.onrender.com']
+ALLOWED_HOSTS = ['mactrex.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
